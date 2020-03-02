14182ca7216a9bd0c15fa0b04c28f1b02f64af17
kasialakocy <k.lakocy@microsoft.wsei.edu.pl>
2020-03-02 18:37:29 +0100

    figury

---
+++ Figury/ConsoleAppFigury/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FiguryLibrary;
+
+namespace ConsoleAppFigury
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Trojkat t1; //deklaracja zmiennej typu trojkat
+            t1 = new Trojkat(); // new - uzywasz zawsze jak chcesz stworzyc obiekt, operator stworzenia obiektu
+            Trojkat t2 = new Trojkat(1, 2, 3);
+            var t3 = new Trojkat(c: 1, b: 2, a: 2);
+
+            Console.WriteLine(t1);
+            Console.WriteLine(t2);
+            Console.WriteLine(t3);
+        }
+    }
+}
---
+++ Figury/FiguryLibrary/Trojkat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FiguryLibrary //namespace-identyfikuje obiekty języka C#, przestrzeń nazw
+{
+    public class Trojkat 
+    {
+        //definiujemy dane obiektu - pola (fields)
+        double a; // this.a
+        double b; // this.b
+        double c; // this.c
+
+
+        //konstruktory - jego głównym zadaniem jest stworzyć obiekt
+
+
+
+        public Trojkat () // kontruktor domyślny, on nie ma argumentów
+        {
+            a = b = c = 1.0;
+
+        }
+
+        public Trojkat(double a, double b, double c)
+        {
+
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+    }
+}
